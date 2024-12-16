0c5b3843d0bd5d419d440c77f8084989dfa515b8
vedanth515 <vedanthkarnataka@gmail.com>
2024-12-16 19:36:02 +0530

    Add files via upload

---
+++ prime.js
@@ -0,0 +1,100 @@
+
+
+let a=+prompt("enter a digit")
+let count=0
+for(i=1;i<=a;i++){
+    if(a%2==0){
+        count++
+    }
+}
+if(count==2){
+    console.log(a,"is prime number");
+    
+}else{
+    console.log(a, "is not a prime number");
+    
+}
+
+
+
+
+let a=+prompt("enter a digit")
+let count=0
+for(i=1;i<=a;i++){
+    if(a%i==0){
+        count++
+    }
+}
+if(count==2){
+    console.log(a,"is prime number");
+    
+}else{
+    console.log(a, "is not a prime number");
+    
+}
+
+
+let a=+prompt("enter a digit")
+let count=1
+for(i=1;i<=a;i++){
+    if(a%i==0){
+        count++
+    }
+}
+if(count==2){
+    console.log(a,"is prime number");
+    
+}else{
+    console.log(a, "is not a prime number");
+    
+}
+
+
+let a=+prompt("enter a digit")
+let count=2
+for(i=1;i<=a;i++){
+    if(a%i==0){
+        count++
+    }
+}
+if(count==2){
+    console.log(a,"is prime number");
+    
+}else{
+    console.log(a, "is not a prime number");
+    
+}
+
+
+let a=+prompt("enter a digit")
+let count=3
+for(i=1;i<=a;i++){
+    if(a%i==0){
+        count++
+    }
+}
+if(count==2){
+    console.log(a,"is prime number");
+    
+}else{
+    console.log(a, "is not a prime number");
+    
+}
+
+
+let a=+prompt("enter a digit")
+let count=0
+if(a>=1){
+ for(i=0;i<=a;i++){
+    if(a%i==0){
+        count++
+    }
+ }
+}
+if(count==2){
+    console.log(a, "is prime number");
+    
+}else{
+    console.log(a,"is not a prime number");
+    
+}
